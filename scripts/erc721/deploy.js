a9298559f89dd038c0429ca27bd939aeff0a1678
Krishang <krishang@thirdweb.com>
2024-02-01 02:40:28 +0530

    Update deploy script

--- scripts/erc721/deploy.js
+++ scripts/erc721/deploy.js
@@ -2,42 +2,35 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { config } from "dotenv";
 import { ethers } from "ethers";
 
+import ERC721_CORE_ABI from "../../abi/ERC721Core.json" assert { type: "json" };
+
 config();
 
-// Uses a clone factory contract to deploy a minimal clone proxy contract for an implementation.
+// Uses a clone factory contract to deploy a minimal clone proxy contract for an ERC-721 Core implementation.
+
+// CHAIN: Goerli
+
 // Run: `node scripts/erc721/deploy.js`
 
 const CLONE_FACTORY_ADDRESS = "0x276681b249D043dfd3e833fA2862B797dA2BF68E";
+const ERC_721_CORE_INTERFACE = new ethers.utils.Interface(ERC721_CORE_ABI);
 
 async function deployProxy(sdkInstance) {
-  // PARAMS
-  const implementation = "0x05bA337AC23E1bdb70aDA414aeA75260bCe6a71d";
-  const admin = "0x2Ee4c2e9666Ff48DE2779EB6f33cDC342d761372";
+  const admin = "0x2Ee4c2e9666Ff48DE2779EB6f33cDC342d761372"; // REPLACE WITH YOUR ADDRESS
 
-  const iface = new ethers.utils.Interface([
-    {
-      type: "function",
-      name: "initialize",
-      inputs: [
-        {
-          name: "_defaultAdmin",
-          type: "address",
-          internalType: "address",
-        },
-        { name: "_name", type: "string", internalType: "string" },
-        { name: "_symbol", type: "string", internalType: "string" },
-        { name: "_uri", type: "string", internalType: "string" },
-      ],
-      outputs: [],
-      stateMutability: "nonpayable",
-    },
-  ]);
-  const initializeData = iface.encodeFunctionData("initialize", [
-    admin,
-    "Thirdweb",
-    "TWB",
-    "",
-  ]);
+  // PARAMS
+  const implementation = "0x7720573Fe31a2f2fe523E24CC4904d0040947FA5";
+  const initializeData = ERC_721_CORE_INTERFACE.encodeFunctionData(
+    "initialize",
+    [
+      { target: ethers.constants.AddressZero, value: 0, data: "0x" },
+      [],
+      admin,
+      "Hooks NFT Collection",
+      "HNC",
+      "",
+    ]
+  );
   const salt = ethers.utils.keccak256(
     ethers.utils.defaultAbiCoder.encode(["address", "uint256"], [admin, 1])
   );
